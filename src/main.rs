75f7991828458ba21c968148697c924609e21fbe
Ciyss <bj856575@gmail.com>
2025-12-19 15:34:49 +0700

    penggunaan features

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use latihan_pelajaran_bahasa_pemograman_rust_pzn_say_hello::{say_hello};
+fn main() {
+    println!("Hello, world {}",say_hello("Kanjut"));
+    // println!("Hello, world {}",say_goodbye("Kanjut"));
+}
